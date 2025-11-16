c92aa596174f58f65a9e392a5f4ee20eb2dbaa40
Catalina Alzamora <alzamoracatalina@gmail.com>
2025-11-16 20:51:14 -0300

    primer commit

---
+++ src/main/java/com/catalinaalzamora/peliculasydirectores/PeliculasydirectoresApplication.java
@@ -0,0 +1,13 @@
+package com.catalinaalzamora.peliculasydirectores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeliculasydirectoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeliculasydirectoresApplication.class, args);
+	}
+
+}
